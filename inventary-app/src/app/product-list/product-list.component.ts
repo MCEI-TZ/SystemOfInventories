2213a839f81a185825b9188f4b68e8811c6269d3
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-03-16 17:44:20 -0600

    feat(component): Add ngOnInit that initialize the petition of obtainProducts

--- inventary-app/src/app/product-list/product-list.component.ts
+++ inventary-app/src/app/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Product } from '../product';
+import { ProductServiceService } from '../product-service.service';
 
 @Component({
   selector: 'app-product-list',
@@ -7,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+  products!: Product[];
 
+  private productService = inject(ProductServiceService);
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.obtainProducts();
+  }
+
+  obtainProducts(): void {
+    this.productService.listProducts().subscribe({
+      next: (data) => {
+        this.products = data;
+      }
+      ,
+      error: (err) => {
+        console.error('Error fetching products:', err);
+      }
+    })
+  }
 }
