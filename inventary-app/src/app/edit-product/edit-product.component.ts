6cde9f16a651fe94c644bb4407b7a193273b6c60
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-03-18 15:55:15 -0600

    feat(component): Add initializer for get the product to update

--- inventary-app/src/app/edit-product/edit-product.component.ts
+++ inventary-app/src/app/edit-product/edit-product.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Product } from '../product';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductServiceService } from '../product-service.service';
 
 @Component({
   selector: 'app-edit-product',
   imports: [],
   templateUrl: './edit-product.component.html',
-  styleUrl: './edit-product.component.css'
+  styleUrl: './edit-product.component.css',
 })
 export class EditProductComponent {
+  product: Product = new Product();
+  id!: number;
 
+  private productService = inject(ProductServiceService);
+  private router = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    this.id = this.router.snapshot.params['id'];
+    this.productService.findById(this.id).subscribe({
+      next: (data) => (this.product = data),
+      error: (err) => {
+        console.error('Error fetching product:', err);
+      },
+    });
+  }
 }
