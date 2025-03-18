f6e5c85be3a33c25a1c8c093bea8de3c25045473
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-03-18 12:07:03 -0600

    feat(routes): Add Router Module and some routes

--- inventary-app/src/app/app.component.ts
+++ inventary-app/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ProductListComponent } from "./product-list/product-list.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [ProductListComponent],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
