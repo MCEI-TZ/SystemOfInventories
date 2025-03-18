b96745f867aefd1cb1f24f324506d753d6723195
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-03-18 15:46:09 -0600

    feat(component): Add method edit and delete Product in the component

--- inventary-app/src/app/product-list/product-list.component.ts
+++ inventary-app/src/app/product-list/product-list.component.ts
@@ -1,17 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { Product } from '../product';
 import { ProductServiceService } from '../product-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   imports: [],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
   products!: Product[];
 
   private productService = inject(ProductServiceService);
+  private router = inject(Router);
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
@@ -23,11 +25,25 @@ export class ProductListComponent {
     this.productService.listProducts().subscribe({
       next: (data) => {
         this.products = data;
-      }
-      ,
+      },
       error: (err) => {
         console.error('Error fetching products:', err);
-      }
-    })
+      },
+    });
+  }
+
+  editProduct(id: number) {
+    this.router.navigate(['/editProduct', id]);
+  }
+
+  deleteProduct(id: number): void {
+    this.productService.deleteProduct(id).subscribe({
+      next: () => {
+        this.obtainProducts();
+      },
+      error: (err) => {
+        console.error('Error deleting product:', err);
+      },
+    });
   }
 }
