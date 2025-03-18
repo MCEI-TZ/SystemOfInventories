64adb8f41d96a1ded34ac57c68f8e91dc37a12ca
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-03-18 15:44:01 -0600

    feat(routes): Add edit path for products

--- inventary-app/src/app/app.routes.ts
+++ inventary-app/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { AddProductComponent } from './add-product/add-product.component';
+import { EditProductComponent } from './edit-product/edit-product.component';
 
 //http://localhost:4200/products/
 export const routes: Routes = [
-  {path: 'products',component: ProductListComponent},
-  {path: '', redirectTo: 'products' , pathMatch:'full'},
-  {path: 'addProduct', component: AddProductComponent}
+  { path: 'products', component: ProductListComponent },
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'addProduct', component: AddProductComponent },
+  { path: 'editProduct/:id', component: EditProductComponent },
 ];
