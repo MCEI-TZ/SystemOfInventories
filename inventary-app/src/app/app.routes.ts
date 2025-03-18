2e5dd6e75dc168eae0d0b2f8f1a0c16fbd2405c5
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-03-18 12:36:38 -0600

    feat(routes): Add path for component of creation of Products

--- inventary-app/src/app/app.routes.ts
+++ inventary-app/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
 //http://localhost:4200/products/
 export const routes: Routes = [
   {path: 'products',component: ProductListComponent},
-  {path: '', redirectTo: 'products' , pathMatch:'full'}
+  {path: '', redirectTo: 'products' , pathMatch:'full'},
+  {path: 'addProduct', component: AddProductComponent}
 ];
